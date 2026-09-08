9ea67a7e70b442fe2bb450d046e142b70affa5f6
Laraib_Fatima <laraibfatimaftaqua@gmail.com>
2026-09-08 12:27:15 +0500

    Clean up header comments and code indentation in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
+/*
+ * Course: CS-250 Data Structures and Algorithms
+ * Lab 1: Task 1 - GitHub Repository Setup
+ * Author: Laraib Fatima
+ * Date: 08-09-2026
+ */
+
 #include <iostream>
 using namespace std;
 
+// Main function
 int main() {
+    // Output message to terminal
     cout << "Welcome to DSA Lab 1!" << endl;
-    return 0;
+    
+    return 0; // Returns 0 to indicate successful execution
 }
